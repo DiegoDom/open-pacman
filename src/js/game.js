fc403ffea46832b446e1d4e50324bf98fc47110e
DiegoDom <dominguezdiex@gmail.com>
2026-09-18 11:50:05 -0600

    Step 1: pellet power

--- src/js/game.js
+++ src/js/game.js
@@ -27,7 +27,7 @@ function createGame() {
   grid[ PACMAN_START.y ][ PACMAN_START.x ] = 0;
 
   let dots = 0;
-  for ( const row of grid ) for ( const v of row ) if ( v === 2 ) dots++;
+  for ( const row of grid ) for ( const v of row ) if ( v === 2 || v === 4 ) dots++;
 
   return {
     state: 'start',
@@ -107,6 +107,12 @@ function movePacman( game ) {
     if ( grid[ p.y ][ p.x ] === 2 ) {
       grid[ p.y ][ p.x ] = 0;
       game.score += 10;
+      game.dotsRemaining--;
+    }
+    // Comer power pellet.
+    if ( grid[ p.y ][ p.x ] === 4 ) {
+      grid[ p.y ][ p.x ] = 0;
+      game.score += 50;
       game.dotsRemaining--;
     }
     // Si no puede seguir, se detiene en la celda.
